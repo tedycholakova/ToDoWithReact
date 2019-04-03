28cd429de3929cafe0e6cb5da7358aee2dc0106f
Teodora Cholakova <tedycholakova@gmail.com>
2019-04-03 18:13:16 +0300

    Removed unnecessary files, added todo component and function for deleting todos.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'Buy some milk' },
+      { id: 2, content: 'Go to gym' },
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const toDos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: toDos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
